74c6cd0878ede53f82d3b09e7250b4ab84e92853
juan hiciano <juanlhiciano@gmail.com>
2020-12-08 21:58:58 -0400

    Template project for Backend
    User Entity CRUD

--- Back-End/BrainServices.Api/BrainServices.Api/Startup.cs
+++ Back-End/BrainServices.Api/BrainServices.Api/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebApi.Extensions;
 
 namespace BrainServices.Api
 {
@@ -28,20 +21,20 @@ namespace BrainServices.Api
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BrainServices.Api", Version = "v1" });
-            });
+            services.ConfigureDbContext(Configuration);
+            services.ServicesImplementations();
+            services.ConfigureAutomapper();
+            services.ConfigureSwagger();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BrainServices.Api v1"));
             }
 
             app.UseHttpsRedirection();
@@ -54,6 +47,7 @@ namespace BrainServices.Api
             {
                 endpoints.MapControllers();
             });
+            app.ConfigureSwaggerMiddleWare();
         }
     }
 }
