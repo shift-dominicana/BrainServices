f2534af41933a1270adef71af5c8b8e8a889d06e
Dany <dannyb1392@gmail.com>
2020-12-10 00:29:19 -0400

    Add Mapping Model

--- Back-End/BrainServices.Api/DataLayer/ViewModels/Users/UserViewModel.cs
+++ Back-End/BrainServices.Api/DataLayer/ViewModels/Users/UserViewModel.cs
@@ -9,6 +9,7 @@ namespace DataLayer.ViewModels.Users
 {
     public class UserViewModel : BaseViewModel
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
